9af9f0348ea4f4f95e34e1fae65a136b7d567d41
versiondown <versiondown@outlook.com>
2020-03-25 15:24:24 +0800

    EE: Book Server's MVP is done. FE: Book Search's MVP is done.

--- fe/src/plugin/apiEas/index.js
+++ fe/src/plugin/apiEas/index.js
@@ -60,9 +60,9 @@ service.interceptors.request.use(
 service.interceptors.response.use(
   response => {
     // dataAxios 是 axios 返回数据中的 data
-    const dataAxios = response.data
+    const dataAxios = response
     // 这个状态码是和后端约定的
-    const { code } = dataAxios
+    const { code } = dataAxios.data
     // 根据 code 进行判断
     if (code === undefined) {
       // 如果没有 code 代表这不是项目后端开发的接口 比如可能是 D2Admin 请求最新版本
@@ -72,14 +72,14 @@ service.interceptors.response.use(
       switch (code) {
         case 0:
           // [ 示例 ] code === 0 代表没有错误
-          return dataAxios.data
+          return dataAxios
         case 'xxx':
           // [ 示例 ] 其它和后台约定的 code
-          errorCreate(`[ code: xxx ] ${dataAxios.msg}: ${response.config.url}`)
+          errorCreate(`[ code: xxx ] ${dataAxios.data.msg}: ${response.config.url}`)
           break
         default:
           // 不是正确的 code
-          errorCreate(`${dataAxios.msg}: ${response.config.url}`)
+          errorCreate(`${dataAxios.data.msg}: ${response.config.url}`)
           break
       }
     }
